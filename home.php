f487892d670ccc8444bea3f379b2aeb6adda8612
Pedro Hugo <pedrohugoferreira@gmail.com>
2023-01-10 16:45:44 -0300

    segurança de autentifição

--- home.php
+++ home.php
@@ -1,3 +1,15 @@
+<?php
+
+  session_start();
+  if(!isset($_SESSION['autenticado']) || $_SESSION['autenticado'] != 'SIM'){
+    header('Location: index.php?login=erro2');
+  }
+
+      
+
+?>
+
+
 <html>
   <head>
     <meta charset="utf-8" />
@@ -18,7 +30,10 @@
 
     <nav class="navbar navbar-dark bg-dark">
       <a class="navbar-brand" href="#">
-        <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+        <img src="logo.png" width="30" heif () {
+    
+    
+  // }ight="30" class="d-inline-block align-top" alt="">
         App Help Desk
       </a>
     </nav>
---
+++ valida_login.php
@@ -0,0 +1,35 @@
+<?php
+
+session_start();
+$_SESSION['x'] = 'Oi, eu sou um valor de sessão!';
+print_r($_SESSION);
+echo '<hr/>';
+print_r($_SESSION['y']);
+
+//variable verificador de autetificação realizada
+
+$usuary_authenticated = false;
+
+//usuary of system
+$usuary_app = array(
+	array('email' => 'adm@teste.com.br', 'password' => '1234567'),
+	array('email' => 'user@teste.com.br', 'password' => 'abcd')
+);
+
+foreach ($usuary_app as $user) {
+
+	if ( $user['email'] === $_POST['email'] && $user['password'] === $_POST['password']) {
+		
+		$usuary_authenticated = true;
+	}
+
+}
+
+if ($usuary_authenticated) {
+ 	echo "Usuário autenticado";
+ 	$_SESSION['autenticado'] = 'SIM';
+} else {
+	$_SESSION['autenticado'] = 'NAO';
+ 	header('Location: index.php?login=erro');
+}
+   
